0934b4dd3779c3a19acaccb5ffc08ea88156c17a
WRudkins <willrudkins@hotmail.com>
2022-07-02 10:28:22 -0400

    Connected to db, Table View working

--- src/main/java/ca/georgian/comp1011m2022assignment2/CountryTableController.java
+++ src/main/java/ca/georgian/comp1011m2022assignment2/CountryTableController.java
@@ -8,11 +8,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.collections.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CountryTableController {
+public class CountryTableController implements Initializable{
 
     @FXML
     private TableColumn<?, ?> CodeCountryTable;
@@ -21,7 +38,7 @@ public class CountryTableController {
     private TableColumn<?, ?> ContinentCountryTable;
 
     @FXML
-    private TableView<?> CountryTable;
+    private TableView<worldDB> CountryTable;
 
     @FXML
     private TableColumn<?, ?> NameCountryTable;
@@ -37,10 +54,23 @@ public class CountryTableController {
 
     @FXML
     public void handleGraphButtonClick(ActionEvent actionEvent) throws IOException {
-        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("PopulationGraph.fxml"));
-        Stage window = (Stage) ViewGraphButton.getScene().getWindow();
-        window.setScene(new Scene(fxmlLoader, 550, 450));
 
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //areaCodeComboBox.getItems().add("All");
+        //areaCodeComboBox.getItems().add("705");
+        CodeCountryTable.setCellValueFactory(new PropertyValueFactory<>("Code"));
+        NameCountryTable.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        ContinentCountryTable.setCellValueFactory(new PropertyValueFactory<>("Continent"));
+        RegionCountryTable.setCellValueFactory(new PropertyValueFactory<>("Region"));
+        PopulationCountryTable.setCellValueFactory(new PropertyValueFactory<>("Population"));
+        CountryTable.getItems().addAll(DBManager.Instance().readCountryTable());
+
+        //numOfStudentsLabel.setText("Number of Students: ");
     }
 
 }
--- src/main/java/ca/georgian/comp1011m2022assignment2/Main.java
+++ src/main/java/ca/georgian/comp1011m2022assignment2/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("student-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CountryTable.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Students!");
+        stage.setTitle("Country Info");
         stage.setScene(scene);
         stage.show();
     }
